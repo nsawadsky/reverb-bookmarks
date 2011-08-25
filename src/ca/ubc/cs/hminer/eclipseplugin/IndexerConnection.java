022847e586d152b5146aac61c34ffa93e3c6b0e6
nsawadsky <nsawadsky@localhost>
2011-08-24 17:35:39 -0700

    WIP

--- src/ca/ubc/cs/hminer/eclipseplugin/IndexerConnection.java
+++ src/ca/ubc/cs/hminer/eclipseplugin/IndexerConnection.java
@@ -13,7 +13,6 @@ import org.codehaus.jackson.util.DefaultPrettyPrinter;
 import ca.ubc.cs.hminer.indexer.messages.IndexerBatchQuery;
 import ca.ubc.cs.hminer.indexer.messages.IndexerMessage;
 import ca.ubc.cs.hminer.indexer.messages.BatchQueryResult;
-import ca.ubc.cs.hminer.study.core.LocationAndClassification;
 
 import npw.NamedPipeWrapper;
 
@@ -64,21 +63,6 @@ public class IndexerConnection implements Runnable {
             callbackId = getNextCallbackId();
             callbacks.put(callbackId, new CallbackInfo(callback, multiShot));
         }
-        StringWriter writer = new StringWriter();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
-        JsonGenerator jsonGenerator = mapper.getJsonFactory().createJsonGenerator(writer);
-        jsonGenerator.setPrettyPrinter(new DefaultPrettyPrinter());
-
-        Class<?> viewClass = Object.class;
-        if (historyMinerData.anonymizePartial) {
-            viewClass = LocationAndClassification.AnonymizePartial.class;
-        }
-        ObjectWriter objWriter = mapper.viewWriter(viewClass);
-        objWriter.writeValue(jsonGenerator, historyReport);
-        
-        return writer.toString();
-
         
     }
     
