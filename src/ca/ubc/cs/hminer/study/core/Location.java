4d7c8d7b4694bddd0a00ff6e516d5efbbde7acfe
nsawadsky <nsawadsky@localhost>
2011-09-09 11:45:41 -0700

    Added comments.

--- src/ca/ubc/cs/hminer/study/core/Location.java
+++ src/ca/ubc/cs/hminer/study/core/Location.java
@@ -2,6 +2,7 @@ package ca.ubc.cs.hminer.study.core;
 
 public class Location {
     public long id;
+    // transient used for fields which must *not* be included in the report that is submitted.
     public transient String url;
     public transient String title;
     public LocationType locationType = LocationType.UNKNOWN;
