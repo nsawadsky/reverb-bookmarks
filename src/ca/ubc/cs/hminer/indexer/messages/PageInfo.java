70edf680c8a4eef36e1073e1e9f42084caea2bed
nsawadsky <nsawadsky@localhost>
2011-09-16 12:37:21 -0700

    PageInfo now implements IndexerMessage.  Also, using different Jackson mechanism for polymorphic serialization/deserialization.

--- src/ca/ubc/cs/hminer/indexer/messages/PageInfo.java
+++ src/ca/ubc/cs/hminer/indexer/messages/PageInfo.java
@@ -1,7 +1,12 @@
 package ca.ubc.cs.hminer.indexer.messages;
 
-public class PageInfo {
+public class PageInfo implements IndexerMessage {
     public PageInfo() {
+    }
+    
+    public PageInfo(String url, String html) {
+        this.url = url;
+        this.html = html;
     }
     
     public String url;
