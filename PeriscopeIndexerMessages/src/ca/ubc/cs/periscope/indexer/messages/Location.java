5b7f3e01c9e5ba25fad334995ae2f15c70b5e818
nsawadsky <nsawadsky@localhost>
2011-10-04 14:09:23 -0700

    Adding frecency boost.

--- PeriscopeIndexerMessages/src/ca/ubc/cs/periscope/indexer/messages/Location.java
+++ PeriscopeIndexerMessages/src/ca/ubc/cs/periscope/indexer/messages/Location.java
@@ -4,11 +4,15 @@ public class Location {
     public Location() {
     }
     
-    public Location(String url, String title) {
+    public Location(String url, String title, float luceneScore) {
         this.url = url;
         this.title = title;
+        this.luceneScore = luceneScore;
     }
     
     public String url;
     public String title;
+    public float luceneScore;
+    public float frecencyBoost;
+    public float overallScore;
 }   
