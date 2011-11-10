055c81b7197aea18d985923f08579949f92fbf38
nsawadsky <nsawadsky@localhost>
2011-11-09 16:05:03 -0800

    Created EclipsePluginTestProject.

---
+++ ReverbEclipsePlugin/EclipsePluginTestProject/src/testpackage/MethodInvoc.java
@@ -0,0 +1,11 @@
+package testpackage;
+
+public class MethodInvoc {
+    public void testMethod() {
+        Methoddecl methoddecl = new Methoddecl();
+        methoddecl.methoddeclMethod();
+        
+        MethoddeclNores methoddeclNores = new MethoddeclNores();
+        methoddeclNores.methoddeclNoresMethod();
+    }
+}
---
+++ ReverbEclipsePlugin/EclipsePluginTestProject/src/testpackage/PrimitiveRefs.java
@@ -0,0 +1,10 @@
+package testpackage;
+
+public class PrimitiveRefs {
+    public void testMethod() {
+        int a = 5;
+        char c = 'c';
+        
+        String test = "test";
+    }
+}
