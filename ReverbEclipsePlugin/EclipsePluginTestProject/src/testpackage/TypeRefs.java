1f72b69dfcc1cfd807cede8205fd8d6cad81e89a
nsawadsky <nsawadsky@localhost>
2011-11-14 09:49:49 -0800

    New test class.

---
+++ ReverbEclipsePlugin/EclipsePluginTestProject/src/testpackage/TypeRefs.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+public class TypeRefs {
+    public void testMethod() {
+        java.io.File qualifiedInst;;
+        
+        java.net.ServerSocket qualifiedInstNores;;
+        
+        Classdecl inst;
+        
+        ClassdeclNores instNores;
+    }
+}
