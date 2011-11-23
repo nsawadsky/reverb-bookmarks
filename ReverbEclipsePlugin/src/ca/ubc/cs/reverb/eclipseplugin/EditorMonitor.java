481abf00976955692508c402e2df7e563bae1665
nsawadsky <nsawadsky@localhost>
2011-11-23 10:08:42 -0800

    Monitor using mouse and keyboard, rather than viewport.

--- ReverbEclipsePlugin/src/ca/ubc/cs/reverb/eclipseplugin/EditorMonitor.java
+++ ReverbEclipsePlugin/src/ca/ubc/cs/reverb/eclipseplugin/EditorMonitor.java
@@ -3,6 +3,8 @@ package ca.ubc.cs.reverb.eclipseplugin;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
@@ -13,21 +15,24 @@ import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextOperationTarget;
 import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.IViewportListener;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IPartListener;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 import ca.ubc.cs.reverb.indexer.messages.BatchQueryResult;
 import ca.ubc.cs.reverb.indexer.messages.IndexerBatchQuery;
 import ca.ubc.cs.reverb.indexer.messages.IndexerQuery;
 
-public class EditorMonitor implements IPartListener, IViewportListener {
-    private final static int REFRESH_DELAY_MSECS = 3000;
+public class EditorMonitor implements IPartListener, MouseListener, KeyListener {
+    private final static int REFRESH_DELAY_MSECS = 1000;
     private final static long INVALID_TIME = -1;
     
     private static EditorMonitor instance = new EditorMonitor();
@@ -36,10 +41,19 @@ public class EditorMonitor implements IPartListener, IViewportListener {
     private IndexerConnection indexerConnection;
     private IWorkbenchPage workbenchPage;
     
+    private ITextViewer lastTextViewer = null;
+    private int lastTopLine = 0;
+    private int lastBottomLine = 0;
+    
     /**
      * Access must be synchronized on the listeners reference.
      */
     private List<EditorMonitorListener> listeners = new ArrayList<EditorMonitorListener>();
+    
+    /**
+     * Set of editor parts we are already listening to.  Access must be synchronized on the listenedParts reference.
+     */
+    private Set<IEditorPart> listenedParts = new HashSet<IEditorPart>();
     
     private EditorMonitor() {
        
@@ -62,8 +76,10 @@ public class EditorMonitor implements IPartListener, IViewportListener {
             workbenchPage.addPartListener(this);
             IWorkbenchPart part = workbenchPage.getActivePart();
             if (part instanceof IEditorPart) {
-                addViewportListener((IEditorPart)part);
-                startRefreshTimer();
+                IEditorPart editorPart = (IEditorPart)part;
+                if (listen(editorPart)) {
+                    handleNavigationEvent(editorPart);
+                }
             }
             isStarted = true;
         }
@@ -84,8 +100,10 @@ public class EditorMonitor implements IPartListener, IViewportListener {
     @Override
     public void partActivated(IWorkbenchPart part) {
         if (part instanceof IEditorPart) {
-            addViewportListener((IEditorPart)part);
-            startRefreshTimer();
+            IEditorPart editorPart = (IEditorPart)part;
+            if (listen(editorPart)) {
+                handleNavigationEvent(editorPart);
+            }
         }
     }
 
@@ -96,7 +114,7 @@ public class EditorMonitor implements IPartListener, IViewportListener {
     @Override
     public void partClosed(IWorkbenchPart part) {
         if (part instanceof IEditorPart) {
-            removeViewportListener((IEditorPart)part);
+            stopListening((IEditorPart)part);
         }
     }
 
@@ -109,84 +127,91 @@ public class EditorMonitor implements IPartListener, IViewportListener {
     } 
     
     @Override
-    public void viewportChanged(int verticalOffset) {
-        startRefreshTimer();
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        handleNavigationEvent();
+    }
+
+    @Override
+    public void mouseDoubleClick(MouseEvent e) {
+        handleNavigationEvent();
+    }
+
+    @Override
+    public void mouseDown(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseUp(MouseEvent e) {
+        handleNavigationEvent();
     }
 
     public void startQuery(IEditorPart editorPart) {
         try {
             if (editorPart != null) {
-                ITextOperationTarget target = (ITextOperationTarget)editorPart.getAdapter(ITextOperationTarget.class);
-                if (target == null) {
-                    throw new PluginException("Failed to get ITextOperationTarget adapter");
-                } 
-                if (!(target instanceof ITextViewer)) {
-                    throw new PluginException("ITextOperationTarget adapter is not instance of ITextViewer");
+                final ITextViewer textViewer = getTextViewer(editorPart);
+                if (textViewer == null) {
+                    throw new PluginException("Failed to get text viewer");
                 }
-                final ITextViewer textViewer = (ITextViewer)target;
-                if (!(editorPart instanceof AbstractTextEditor)) {
-                    throw new PluginException("Editor part is not instance of AbstractTextEditor");
-                } 
-                IEditorInput editorInput = editorPart.getEditorInput();
-                if (editorInput == null) {
-                    throw new PluginException("Editor input is null");
-                } 
-                final IJavaElement javaElement = JavaUI.getEditorInputJavaElement(editorInput);
-                if (javaElement == null) {
-                    throw new PluginException("Failed to get Java element from editor input");
-                } 
-                if (!(javaElement instanceof ICompilationUnit)) {
-                    throw new PluginException("Editor input Java element is not instance of ICompilationUnit");
-                } 
+                final ICompilationUnit compileUnit = getCompilationUnit(editorPart);
+                if (compileUnit == null) {
+                    throw new PluginException("Failed to get compilation unit");
+                }
                 final int topLine = textViewer.getTopIndex();
                 final int bottomLine = textViewer.getBottomIndex();
-                Runnable updateViewTask = new Runnable() {
-
-                    @Override
-                    public void run() {
-                        IDocument doc = textViewer.getDocument();
-                        try {
-                            int topPosition = doc.getLineOffset(topLine);
-                            int bottomPosition = doc.getLineOffset(bottomLine) + doc.getLineLength(bottomLine) - 1;
-                            buildAndExecuteQuery((ICompilationUnit)javaElement, 
-                                    topPosition, bottomPosition);
-                        } catch (Exception e) {
-                            getLogger().logError("Error creating/executing query", e);
-                        }
-                    }
+                
+                if (!textViewer.equals(lastTextViewer) || topLine != lastTopLine || bottomLine != lastBottomLine) {
+                    lastTextViewer = textViewer;
+                    lastTopLine = topLine;
+                    lastBottomLine = bottomLine;
                     
-                };
-                Thread updateViewThread = new Thread(updateViewTask);
-                updateViewThread.setPriority(Thread.MIN_PRIORITY);
-                updateViewThread.start();
+                    Runnable updateViewTask = new Runnable() {
+
+                        @Override
+                        public void run() {
+                            IDocument doc = textViewer.getDocument();
+                            try {
+                                int topPosition = doc.getLineOffset(topLine);
+                                int bottomPosition = doc.getLineOffset(bottomLine) + doc.getLineLength(bottomLine) - 1;
+                                buildAndExecuteQuery(compileUnit, topPosition, bottomPosition);
+                            } catch (Exception e) {
+                                getLogger().logError("Error creating/executing query", e);
+                            }
+                        }
+                        
+                    };
+                    Thread updateViewThread = new Thread(updateViewTask);
+                    updateViewThread.setPriority(Thread.MIN_PRIORITY);
+                    updateViewThread.start();
+                }
             }
         } catch (PluginException e) {
             getLogger().logError(e.getMessage(), e);
         }
     }
     
-    private void addViewportListener(IEditorPart editorPart) {
-        ITextOperationTarget target = (ITextOperationTarget)editorPart.getAdapter(ITextOperationTarget.class);
-        if (target == null) {
-            return;
-        } 
-        if (!(target instanceof ITextViewer)) {
-            return;
-        }
-        ITextViewer textViewer = (ITextViewer)target;
-        textViewer.addViewportListener(this);
-    }
-    
-    private void removeViewportListener(IEditorPart editorPart) {
-        ITextOperationTarget target = (ITextOperationTarget)editorPart.getAdapter(ITextOperationTarget.class);
-        if (target == null) {
-            return;
-        } 
-        if (!(target instanceof ITextViewer)) {
-            return;
-        }
-        ITextViewer textViewer = (ITextViewer)target;
-        textViewer.removeViewportListener(this);
+    private void buildAndExecuteQuery(ICompilationUnit compilationUnit, 
+            int topPosition, int bottomPosition) throws InterruptedException, IOException {
+        ASTParser parser = ASTParser.newParser(AST.JLS3);
+        parser.setSource(compilationUnit);
+        parser.setResolveBindings(true);
+        parser.setStatementsRecovery(true);
+        CompilationUnit compileUnit = (CompilationUnit)parser.createAST(null);
+        QueryBuilderASTVisitor visitor = new QueryBuilderASTVisitor(compileUnit.getAST(), topPosition, bottomPosition);
+        compileUnit.accept(visitor);
+        
+        final BatchQueryResult result = indexerConnection.runQuery(new IndexerBatchQuery(visitor.getQueries()), 20000);
+        
+        PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+
+            @Override
+            public void run() {
+                notifyListeners(result);
+            }
+        });
     }
     
     private void startRefreshTimer() {
@@ -202,7 +227,7 @@ public class EditorMonitor implements IPartListener, IViewportListener {
                 boolean restart = false;
                 if (lastRefreshTime != this.startTime) {
                     int newDelay = (int)(lastRefreshTime + REFRESH_DELAY_MSECS - System.currentTimeMillis());
-                    if (newDelay >= 500) {
+                    if (newDelay >= 100) {
                         // Refresh requested since this timer was started, restart the timer.
                         restart = true;
                         this.startTime = lastRefreshTime;
@@ -226,28 +251,59 @@ public class EditorMonitor implements IPartListener, IViewportListener {
         }
     }
 
-    private void buildAndExecuteQuery(ICompilationUnit compilationUnit, 
-            int topPosition, int bottomPosition) throws InterruptedException, IOException {
-        ASTParser parser = ASTParser.newParser(AST.JLS3);
-        parser.setSource(compilationUnit);
-        parser.setResolveBindings(true);
-        parser.setStatementsRecovery(true);
-        CompilationUnit compileUnit = (CompilationUnit)parser.createAST(null);
-        QueryBuilderASTVisitor visitor = new QueryBuilderASTVisitor(compileUnit.getAST(), topPosition, bottomPosition);
-        compileUnit.accept(visitor);
-        
-        List<IndexerQuery> queries = visitor.getQueries();
-        //logQueries(queries);
-        
-        final BatchQueryResult result = indexerConnection.runQuery(new IndexerBatchQuery(visitor.getQueries()), 20000);
-        
-        PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+    private void handleNavigationEvent() {
+        IEditorPart editorPart = workbenchPage.getActiveEditor();
+        if (editorPart != null) {
+            handleNavigationEvent(editorPart);
+        }
+    }
+    
+    private void handleNavigationEvent(IEditorPart editorPart) {
+        startRefreshTimer();
+    }
+    
+    private boolean listen(IEditorPart editorPart) {
+        if (isListenedPart(editorPart)) {
+            return true;
+        }
+        if (getTextViewer(editorPart) != null && getCompilationUnit(editorPart) != null) {
+            Control control = (Control)editorPart.getAdapter(Control.class);
+            control.addKeyListener(this);
+            control.addMouseListener(this);
+            addToListenedParts(editorPart);
+            return true;
+        }
+        return false;
+    }
+    
+    private void stopListening(IEditorPart editorPart) {
+        if (isListenedPart(editorPart)) {
+            Control control = (Control)editorPart.getAdapter(Control.class);
+            control.removeKeyListener(this);
+            control.removeMouseListener(this);
 
-            @Override
-            public void run() {
-                notifyListeners(result);
-            }
-        });
+            removeFromListenedParts(editorPart);
+        }
+    }
+    
+    private ITextViewer getTextViewer(IEditorPart editorPart) {
+        ITextOperationTarget target = (ITextOperationTarget)editorPart.getAdapter(ITextOperationTarget.class);
+        if (!(target instanceof ITextViewer)) {
+            return null;
+        }
+        return (ITextViewer)target;
+    }
+    
+    private ICompilationUnit getCompilationUnit(IEditorPart editorPart) {
+        IEditorInput editorInput = editorPart.getEditorInput();
+        if (editorInput == null) {
+            return null;
+        } 
+        IJavaElement javaElement = JavaUI.getEditorInputJavaElement(editorInput);
+        if (!(javaElement instanceof ICompilationUnit)) {
+            return null;
+        }
+        return (ICompilationUnit)javaElement;
     }
     
     private void notifyListeners(BatchQueryResult result) {
@@ -261,6 +317,24 @@ public class EditorMonitor implements IPartListener, IViewportListener {
             } catch (Throwable t) {
                 getLogger().logError("Listener threw exception", t);
             }
+        }
+    }
+    
+    private void addToListenedParts(IEditorPart editorPart) {
+        synchronized(listenedParts) {
+            listenedParts.add(editorPart);
+        }
+    }
+    
+    private void removeFromListenedParts(IEditorPart editorPart) {
+        synchronized(listenedParts) {
+            listenedParts.remove(editorPart);
+        }
+    }
+    
+    private boolean isListenedPart(IEditorPart editorPart) {
+        synchronized(listenedParts) {
+            return listenedParts.contains(editorPart);
         }
     }
     
