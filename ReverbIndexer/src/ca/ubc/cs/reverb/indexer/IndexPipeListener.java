6c1a5e8f4b2e5ca13ef43644cbc772f7d551093d
nsawadsky <nsawadsky@localhost>
2011-11-29 11:08:38 -0800

    Moved periodic commit to separate thread.

--- ReverbIndexer/src/ca/ubc/cs/reverb/indexer/IndexPipeListener.java
+++ ReverbIndexer/src/ca/ubc/cs/reverb/indexer/IndexPipeListener.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
-import xpnp.TimeoutException;
 import xpnp.XpNamedPipe;
 
 import org.apache.log4j.Logger;
@@ -15,6 +14,7 @@ import ca.ubc.cs.reverb.indexer.messages.UpdatePageInfoRequest;
 
 public class IndexPipeListener implements Runnable {
     private static Logger log = Logger.getLogger(IndexPipeListener.class);
+    private final static int COMMIT_INTERVAL_MSECS = 30000;
 
     private IndexerConfig config;
     private WebPageIndexer indexer;
@@ -32,6 +32,25 @@ public class IndexPipeListener implements Runnable {
             throw new IndexerException("Failed to create index pipe: " + e, e);
         }
         new Thread(this).start();
+        
+        // Start a thread which periodically commits changes.
+        class IndexCommitter implements Runnable {
+
+            @Override
+            public void run() {
+                Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+                while (true) {
+                    try {
+                        Thread.sleep(COMMIT_INTERVAL_MSECS);
+                        indexer.commitChanges();
+                    } catch (Exception e) {
+                        log.error("Error committing changes", e);
+                    }
+                }
+            }
+        }
+        
+        new Thread(new IndexCommitter()).start();
     }
     
     public void run() {
@@ -47,8 +66,6 @@ public class IndexPipeListener implements Runnable {
     }
    
     private class IndexPipeConnection implements Runnable {
-        private final static int COMMIT_INTERVAL_MSECS = 30000;
-        
         private IndexerConfig config;
         private XpNamedPipe pipe;
         private WebPageIndexer indexer;
@@ -62,44 +79,31 @@ public class IndexPipeListener implements Runnable {
         public void run() {
             try {
                 Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
-                long nextCommitTime = System.currentTimeMillis() + COMMIT_INTERVAL_MSECS;
 
                 ObjectMapper mapper = new ObjectMapper();
                 while (true) {
-                    long timeToWait = nextCommitTime - System.currentTimeMillis();
-                    if (timeToWait <= 100) {
-                        try {
-                            indexer.commitChanges();
-                        } catch (IndexerException e) {
-                            log.error("Error committing index changes", e);
-                        }
-                        nextCommitTime = System.currentTimeMillis() + COMMIT_INTERVAL_MSECS;
-                        timeToWait = COMMIT_INTERVAL_MSECS;
-                    }
+                    byte[] data = pipe.readMessage();
+                    UpdatePageInfoRequest info = null;
                     try {
-                        byte[] data = pipe.readMessage((int)timeToWait);
-                        UpdatePageInfoRequest info = null;
+                        IndexerMessageEnvelope envelope = mapper.readValue(data, IndexerMessageEnvelope.class);
+                        if (envelope.message == null) {
+                            throw new IndexerException("envelope.message is null");
+                        }
+                        if (!(envelope.message instanceof UpdatePageInfoRequest)) {
+                            throw new IndexerException("Unexpected message content: " + envelope.message.getClass());
+                        }
+                        info = (UpdatePageInfoRequest)envelope.message;
+                    } catch (Exception e) {
+                        log.error("Exception parsing message from index pipe", e);
+                    }
+                    if (info != null) {
+                        log.info("Got page: " + info.url);
                         try {
-                            IndexerMessageEnvelope envelope = mapper.readValue(data, IndexerMessageEnvelope.class);
-                            if (envelope.message == null) {
-                                throw new IndexerException("envelope.message is null");
-                            }
-                            if (!(envelope.message instanceof UpdatePageInfoRequest)) {
-                                throw new IndexerException("Unexpected message content: " + envelope.message.getClass());
-                            }
-                            info = (UpdatePageInfoRequest)envelope.message;
-                        } catch (Exception e) {
-                            log.error("Exception parsing message from index pipe", e);
+                            indexer.indexPage(info);
+                        } catch (IndexerException e) {
+                            log.error("Error indexing page '" + info.url + "'", e);
                         }
-                        if (info != null) {
-                            log.info("Got page: " + info.url);
-                            try {
-                                indexer.indexPage(info);
-                            } catch (IndexerException e) {
-                                log.error("Error indexing page '" + info.url + "'", e);
-                            }
-                        }
-                    } catch (TimeoutException e) { }
+                    }
                 }
             } catch (IOException e) {
                 log.info("Error reading index pipe", e);
