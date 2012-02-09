a5d863861e68bce308991196c05d5dd1e2889edf
nsawadsky <nsawadsky@localhost>
2012-02-09 11:01:53 -0800

    Removing unused fields and imports.

--- ReverbIndexerMessages/src/ca/ubc/cs/reverb/indexer/messages/CodeQueryReply.java
+++ ReverbIndexerMessages/src/ca/ubc/cs/reverb/indexer/messages/CodeQueryReply.java
@@ -1,27 +1,16 @@
 package ca.ubc.cs.reverb.indexer.messages;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class CodeQueryReply extends IndexerReply {
     public CodeQueryReply() {
     }
     
-    public CodeQueryReply(long timestamp) {
-        this.timestamp = timestamp;
-    }
-    
     public CodeQueryReply(boolean errorOccurred, String errorMessage) {
         super(errorOccurred, errorMessage);
-        timestamp = System.currentTimeMillis();
     }
 
     public List<CodeQueryResult> queryResults = new ArrayList<CodeQueryResult>();
     public List<CodeElementError> errorElements = new ArrayList<CodeElementError>();
-    
-    /**
-     * Timestamp when the results were generated.
-     */
-    public long timestamp;
 }
