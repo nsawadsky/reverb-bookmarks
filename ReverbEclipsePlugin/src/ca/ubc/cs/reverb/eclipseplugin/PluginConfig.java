2d6f30847bf60550425b5b0317b2ee2f2ddbfaaa
nsawadsky <nsawadsky@localhost>
2012-02-09 16:40:28 -0800

    Removing unused imports.

--- ReverbEclipsePlugin/src/ca/ubc/cs/reverb/eclipseplugin/PluginConfig.java
+++ ReverbEclipsePlugin/src/ca/ubc/cs/reverb/eclipseplugin/PluginConfig.java
@@ -3,7 +3,6 @@ package ca.ubc.cs.reverb.eclipseplugin;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.UUID;
 
