483a9793632b38e000f8a4e2b82d4fd81493a76a
nsawadsky <nsawadsky@localhost>
2012-02-14 09:51:12 -0800

    Minor fixes.

--- ReverbEclipsePlugin/src/ca/ubc/cs/reverb/eclipseplugin/views/UploadLogsDialog.java
+++ ReverbEclipsePlugin/src/ca/ubc/cs/reverb/eclipseplugin/views/UploadLogsDialog.java
@@ -78,11 +78,7 @@ public class UploadLogsDialog extends TrayDialog implements SelectionListener {
         lblReverbIsReady = new Label(container, SWT.WRAP);
         String buttonText = "Reverb is ready to upload scrubbed usage logs.  Recommendations and browser page visits are " +
                 "identified in the logs by numbers only.  No code or web page details are included.";
-        if (uploadIndex == totalUploads) {
-            buttonText += "\n\nThis is the final upload.";
-        } else {
-            buttonText += String.format("\n\nThis is upload %d of %d.", uploadIndex, totalUploads);
-        }
+        buttonText += String.format("\n\nThis is upload %d of %d.", uploadIndex, totalUploads);
                 
         lblReverbIsReady.setText(buttonText);
         lblReverbIsReady.setBounds(10, 10, 424, 48);
