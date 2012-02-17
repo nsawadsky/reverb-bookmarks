66d4fc31f3c3b4157b18e26543d81de09ab8660b
nsawadsky <nsawadsky@localhost>
2012-02-17 15:56:48 -0800

    Adding interactivity to installer.

--- ReverbIndexer/src/ca/ubc/cs/reverb/indexer/IndexerService.java
+++ ReverbIndexer/src/ca/ubc/cs/reverb/indexer/IndexerService.java
@@ -1,5 +1,6 @@
 package ca.ubc.cs.reverb.indexer;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,11 +16,16 @@ import org.apache.log4j.spi.RootLogger;
 import org.apache.lucene.index.FieldInvertState;
 import org.apache.lucene.search.DefaultSimilarity;
 import org.apache.lucene.search.Similarity;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import xpnp.XpNamedPipe;
 
 import ca.ubc.cs.reverb.indexer.messages.BatchQueryReply;
+import ca.ubc.cs.reverb.indexer.messages.IndexerMessageEnvelope;
 import ca.ubc.cs.reverb.indexer.messages.IndexerQuery;
 import ca.ubc.cs.reverb.indexer.messages.Location;
 import ca.ubc.cs.reverb.indexer.messages.QueryResult;
+import ca.ubc.cs.reverb.indexer.messages.ShutdownRequest;
 import ca.ubc.cs.reverb.indexer.study.StudyDataCollector;
 
 public class IndexerService {
@@ -40,7 +46,22 @@ public class IndexerService {
         try {
             config = new IndexerConfig();
 
+            IndexerArgsInfo argsInfo = parseArgs(args);
+            
+            if (argsInfo.mode == IndexerMode.UNINSTALL) {
+                unregisterAndShutdownService();
+                log.info("Unregistered and shutdown service successfully");
+                return;
+            }
+
+            if (argsInfo.mode == IndexerMode.INSTALL){
+                installService();
+                return;
+            }
+            
             // We need config to be initialized before we can configure file logging.
+            // Also, for certain startup modes, we do not want to compete with the main service
+            // for access to the log file.
             configureFileDebugLogging();
             
             configLucene();
@@ -51,25 +72,24 @@ public class IndexerService {
             locationsDatabase = new LocationsDatabase(config);
             indexer = new WebPageIndexer(config, locationsDatabase, collector);
             
-            Map<String, String> parsed = parseArgs(args);
-            String query = parsed.get("-query");
-            if (query != null) {
-                runQuery(config, indexer, locationsDatabase, collector, query);
-            } else {
-                indexer.startCommitter();
-                
-                IndexPipeListener indexPipeListener = new IndexPipeListener(this, config, indexer);
-                
-                QueryPipeListener queryPipeListener = new QueryPipeListener(config, indexer, locationsDatabase, collector);
-                
-                indexPipeListener.start();
-                queryPipeListener.start();
-                
-                Runtime runtime = Runtime.getRuntime();
-                log.info("Indexer service started, user ID = " + config.getUserId() + ", key = " + config.getUserIdKey() + ", max memory = " + runtime.maxMemory()/(1024*1024) + " MB");
+            if (argsInfo.mode == IndexerMode.QUERY) {
+                runQuery(config, indexer, locationsDatabase, collector, argsInfo.query);
+                return;
             }
+            
+            indexer.startCommitter();
+            
+            IndexPipeListener indexPipeListener = new IndexPipeListener(this, config, indexer);
+            
+            QueryPipeListener queryPipeListener = new QueryPipeListener(config, indexer, locationsDatabase, collector);
+            
+            indexPipeListener.start();
+            queryPipeListener.start();
+            
+            Runtime runtime = Runtime.getRuntime();
+            log.info("Indexer service started, user ID = " + config.getUserId() + ", key = " + config.getUserIdKey() + ", max memory = " + runtime.maxMemory()/(1024*1024) + " MB");
         } catch (Throwable t) {
-            log.error("Exception starting service", t);
+            log.error("Exception invoking service", t);
             System.exit(1);
         }
     }
@@ -154,16 +174,83 @@ public class IndexerService {
         
     }
     
-    private Map<String, String> parseArgs(String[] args) {
-        Map<String, String> result = new HashMap<String, String>();
-        for (String arg: args) {
-            String[] tokenized = arg.split("=");
-            if (tokenized.length != 2) {
-                break;
+    private void installService() throws IndexerException {
+        try { 
+            unregisterAndShutdownService();
+        } catch (IndexerException e) { }
+        
+        
+        
+    }
+    
+    private void unregisterAndShutdownService() throws IndexerException {
+        File installPointer = new File(config.getIndexerInstallPointerPath());
+        if (installPointer.exists()) {
+            if (!installPointer.delete()) {
+                throw new IndexerException("Failed to delete indexer install pointer file");
             }
-            result.put(tokenized[0], tokenized[1]);
         }
-        return result;
+        XpNamedPipe pipe = null;
+        try {
+            pipe = XpNamedPipe.openNamedPipe("reverb-index", true);
+            IndexerMessageEnvelope envelope = new IndexerMessageEnvelope(null, new ShutdownRequest());
+
+            ObjectMapper mapper = new ObjectMapper();
+            byte [] jsonData = mapper.writeValueAsBytes(envelope);
+            
+            pipe.writeMessage(jsonData);
+        } catch (Exception e) {
+            throw new IndexerException("Error sending shutdown request to indexer service: " + e, e);
+        } finally {
+            if (pipe != null) {
+                pipe.close();
+            }
+        }
+        
+    }
+    
+    private IndexerArgsInfo parseArgs(String[] args) {
+        Map<String, String> argsMap = new HashMap<String, String>();
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (arg.startsWith("-")) {
+                arg = arg.substring(1);
+                String value = null;
+                if (i + 1 < args.length && !args[i+1].startsWith("-")) {
+                    value = args[i+1];
+                    i++;
+                }
+                argsMap.put(arg, value);
+            }
+        }
+        if (argsMap.containsKey("query")) {
+            IndexerArgsInfo result = new IndexerArgsInfo(IndexerMode.QUERY);
+            result.query = argsMap.get("query");
+            return result;
+        }
+        if (argsMap.containsKey("install")) {
+            return new IndexerArgsInfo(IndexerMode.INSTALL);
+        }
+        if (argsMap.containsKey("uninstall")) {
+            return new IndexerArgsInfo(IndexerMode.UNINSTALL);
+        }
+        return new IndexerArgsInfo(IndexerMode.NORMAL);
+    }
+    
+    private enum IndexerMode {
+        NORMAL,
+        QUERY,
+        INSTALL,
+        UNINSTALL
+    }
+    
+    private class IndexerArgsInfo {
+        IndexerArgsInfo(IndexerMode mode) {
+            this.mode = mode;
+        }
+        
+        String query;
+        IndexerMode mode;
     }
     
 }
