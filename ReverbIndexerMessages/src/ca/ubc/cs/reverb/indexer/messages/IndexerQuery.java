ac95eb7af7bff31c5fed7255cac856d2c24f56e5
nsawadsky <nsawadsky@localhost>
2012-03-09 14:30:51 -0800

    Reverted.

--- ReverbIndexerMessages/src/ca/ubc/cs/reverb/indexer/messages/IndexerQuery.java
+++ ReverbIndexerMessages/src/ca/ubc/cs/reverb/indexer/messages/IndexerQuery.java
@@ -1,15 +1,13 @@
 package ca.ubc.cs.reverb.indexer.messages;
 
-import java.util.Map;
-import java.util.TreeMap;
-
 public class IndexerQuery {
     public String queryString;
-    public Map<String, String> queryClientInfo = new TreeMap<String, String>();
+    public String queryClientInfo;
     
     public IndexerQuery() { }
 
-    public IndexerQuery(String queryString) {
+    public IndexerQuery(String queryString, String queryClientInfo) {
         this.queryString = queryString;
+        this.queryClientInfo = queryClientInfo;
     }
 }
