65a32ed5b026fb7ce30a89b0380a8da92908bca6
nsawadsky <nsawadsky@localhost>
2012-05-31 17:42:52 -0700

    Added WebPageDownloader.java (for example in Reverb paper).

---
+++ ReverbIndexer/test/ca/ubc/cs/reverb/indexer/WebPageDownloader.java
@@ -0,0 +1,38 @@
+package ca.ubc.cs.reverb.indexer;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class WebPageDownloader {
+    
+    public String getPageContent(String url) throws IOException {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        HttpResponse response = httpClient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+            return EntityUtils.toString(entity);
+        }    
+        return null;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
